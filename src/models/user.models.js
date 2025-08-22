48c1f043c858ac79a0dd02bf43c7743b29facb57
Abhishek-Yadav <yadavabhishek7303@gmail.com>
2025-08-22 17:04:23 +0530

    feat: schema of user

---
+++ src/models/user.models.js
@@ -0,0 +1,69 @@
+import bcrypt from "bcryptjs";
+import crypto from "crypto"
+import jwt from "jsonwebtoken"
+import mongoose, { Schema, schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        avatar: {
+            type: {
+                url: String,
+                localPath: String,
+            },
+            default: {
+                url: `https://via.placeholder.com/200x200.png`,
+                localPath: "",
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        isEmailVerified: {
+            type: Boolean,
+            default: false,
+        },
+        refreshToken: {
+            type: String,
+        },
+        forgotPasswordToken: {
+            type: String,
+        },
+        forgotPasswordExpiry: {
+            type: Date,
+        },
+        emailVerificationToken: {
+            type: String,
+        },
+        emailVerificationExpiry: {
+            type: Date,
+        },
+    },
+    { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+}
+);
